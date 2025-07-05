fd8c0270d21e55f0a5b670599079dc52174549c6
Sage <folorunshomubiboy@gmail.com>
2025-07-06 00:27:03 +0100

    debug: add red background and force 100vw to find white edge culprit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#fffbe6" />
       </head>
       <body
-        className="w-full min-w-0 overflow-x-hidden font-amiri bg-gradient-to-b from-green-50 to-white text-gray-800 min-h-screen ${geistSans.variable} ${geistMono.variable} antialiased"
+        className="w-[100vw] min-h-screen bg-red-200 overflow-x-hidden m-0 p-0 font-amiri bg-gradient-to-b from-green-50 to-white text-gray-800 min-h-screen ${geistSans.variable} ${geistMono.variable} antialiased"
       >
         {children}
       </body>
